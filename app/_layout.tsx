3c422e03fdbe925441c162098570bedffc1e1ee1
luispolonio1 <lpoloniol@unemi.edu.ec>
2026-05-20 11:20:10 -0500

    git commit -m "creacion de proyecto y subida la rama master"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import './../global.css';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Text, View } from "react-native";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return(
+      <View className='flex-1 justify-center items-center bg-sky-900'>
+          <Text className='text-3xl font-bold'>Hola</Text>
+      </View>
+    )
 }
+
+
